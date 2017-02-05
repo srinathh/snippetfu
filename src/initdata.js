97f552397c780d07873db3bbaf6f28f3465fe766
Hariharan Srinath <srinathh@gmail.com>
2017-02-05 11:57:59 +0530

    added copyright messages and removed unused files

--- src/initdata.js
+++ src/initdata.js
@@ -1,3 +1,21 @@
+/*
+Snippet-Fu, Copyright (C) 2017, Hariharan Srinath
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 export var initdata = [
     {snippetKey:"be0ed1fb-6d66-4836-8885-094a088319eb", text:"This is a snippet. Click on the it to copy its text to clipboard."},
     {snippetKey:"6a91c45e-6230-47e0-bfcb-9e813321dbee", text:"Add new snippets by clicking the plus icon below."},
